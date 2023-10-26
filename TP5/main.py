7440fdb0dceaa5ed35cc3e6ca1087762cbafeb26
Alvaro Rubina <alvarorubina132@gmail.com>
2023-10-26 08:28:55 -0300

    Actualizado ejercicio 1

--- TP5/main.py
+++ TP5/main.py
@@ -1,7 +1,5 @@
 from funciones import *
 
-import funciones
-
 """1.	Escribir una función que, dado un número de DNI, retorne True si el número es válido y False si no lo es.
 Para que un número de DNI sea válido debe tener entre 7 y 8 dígitos."""
 # Ejercicio 1
