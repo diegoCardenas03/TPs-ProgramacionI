16a4bf384b49d5eb25942ba39c43dda8f44fc9b8
Alvaro Rubina <alvarorubina132@gmail.com>
2023-10-28 16:40:41 -0300

    Trabajo Practico 7 | Actualización

--- Trabajo_Practico_7/TP7.py
+++ Trabajo_Practico_7/TP7.py
@@ -3,3 +3,13 @@ año de publicación, etc.). Luego, escribe un programa que ordene la lista de l
 específico, como el año de publicación o el autor."""
 
 # Ejercicio 3
+
+# Lista con diccionarios, cada diccionario contiene informacion sobre X libro
+dictionary_list =  [{"name": "El código Da Vinci", "author": "Dan Brown", "year": 2003}, 
+    {"name": "El gran Gatsby", "author": "Francis Scott Key Fitzgerald " ,"year": 1925},
+    {"name": "El niño con el pijama de rayas", "author": "John Boyne", "year": 2006},
+    {"name": "La naranja mecánica", "author": "Anthony Burgess", "year": 1962},
+    {"name": "Cien años de soledad", "author": "Gabriel García Marquéz", "year": 1967},
+    {"name": "Crimen y castigo", "author": " Fyodor Dostoevsky", "year": 1866}]   
+
+# Ordenamiento de los libros segun el campo (proximo a desarrollar)
