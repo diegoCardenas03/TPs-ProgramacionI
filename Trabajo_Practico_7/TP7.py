2697461aaaaa1b639e2aa5bc9842e9126595213b
Alvaro Rubina <alvarorubina132@gmail.com>
2023-10-28 15:06:05 -0300

    Trabajo Practico 7

---
+++ Trabajo_Practico_7/TP7.py
@@ -0,0 +1,5 @@
+"""3.	Crea una lista de diccionarios, donde cada diccionario contiene información sobre un libro (título, autor,
+año de publicación, etc.). Luego, escribe un programa que ordene la lista de libros en función de un campo 
+específico, como el año de publicación o el autor."""
+
+# Ejercicio 3
