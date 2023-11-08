d4294a6d5cdab10e3ae7b7a9e1571a316b101a9a
Alvaro Rubina <alvarorubina132@gmail.com>
2023-11-08 13:58:07 -0300

    Clase | Metodos actualizados

--- Ejercicios_complementarios/ejercicio_clases_objetos/motocicleta.py
+++ Ejercicios_complementarios/ejercicio_clases_objetos/motocicleta.py
@@ -14,3 +14,31 @@ class Motocicleta:
         self.manufacture_date = manufacture_date
         self.top_speed = top_speed
         self.weight = weight
+
+    # Métodos
+    def start(self):
+        if self.engine == False:
+            self.engine = True
+            print("-- El motor se ha puesto en marcha --")
+        else:
+            print("-- El motor ya está en marcha, no es necesario arrancarlo nuevamente! --")
+
+    def stop(self):
+        if self.engine == True:
+            self.engine = False
+            print("-- Se ha detenido el motor --")
+        else:
+            print("-- El motor ya está detenido, no es necesario detenerlo nuevamente! --")
+
+    # Metodo para mostrar la motocicleta
+    def showing_motorcycle(self):
+        print(f"\n<< {self.brand.upper()}, Modelo {self.model} >>")
+        print(f"- Año de fabricación: {self.manufacture_date}")
+        print(f"- Peso: {self.weight} Kg")
+        print(f"- Capacidad de combustible: {self.fuel_liters}L")
+        print(f"- Tope de velocidad: {self.top_speed} Km/h")
+        print(f"- Color: {self.color}")
+        print(f"- Numero de ruedas: {self.wheels}")
+        print(f"\n---- PATENTE: [{self.license_plate}]")
+        print(f"---- ESTADO: {self.condition}")
+
